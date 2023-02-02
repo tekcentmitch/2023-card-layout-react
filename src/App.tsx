338de3564234c3bc11df24ec9e6c056a92a31930
Mitch <123044501+tekcentmitch@users.noreply.github.com>
2023-02-02 23:37:34 +1300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.scss";
+
+// API
+const API = "https://mocki.io/v1/f8d44009-0a6d-45ca-8f0a-7a81f2cc0731";
+
+/*
+  API returns an array of "products"
+  Each individual product has the followings:
+    id: string
+    stock: number
+    thumbnail: string
+    title: string
+    brand: string
+    description: string
+    category: string
+    price: number
+*/
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Update Code Here</h1>
+    </div>
+  );
+}
+
+export default App;
